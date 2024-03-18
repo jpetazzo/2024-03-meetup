237208323c4c0603e1d944af2339c53b3b3fb62f
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2024-03-18 15:01:21 +0100

    🏭 Refactor to avoid hard-coded username+password
    Instead of hard-coding the PostgreSQL and RabbitMQ username
    and password, we let their respective operators generate
    a username and safe password. Then we inject that into
    Benthos configuration by using Helmfile templating.
    
    The result doesn't look super pretty so I'm kind of on
    the fence about it, though.

--- postgres/psql.sh
+++ postgres/psql.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+URI="$(kubectl get secrets db-app -o json | jq -r .data.uri | base64 -d)"
 kubectl run --rm -it psql --image nixery.dev/shell/postgresql \
--- psql postgres://enix:password@db-rw/messages
+-- psql $URI
