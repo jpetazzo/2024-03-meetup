795bc6a865d3c4ff6be76210f432f09fb86580d7
xavki <xavki@moi.me>
2024-03-20 20:26:37 +0100

    add a variable to set the producer frequency

--- gui-port-forward.sh
+++ gui-port-forward.sh
@@ -3,4 +3,4 @@
 
 kubectl port-forward -n promstack service/promstack-grafana 8080:80 &
 kubectl port-forward -n promstack service/prometheus-operated 8081:9090 &
-kubectl port-forward -n default service/mq 8082:15672 &
+kubectl port-forward -n v2 service/mq 8082:15672 &
